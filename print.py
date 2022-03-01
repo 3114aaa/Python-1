3dc15ccd8d97ce8c05ebcca11017ee10e339b47a
Luo <95136111+3114aaa@users.noreply.github.com>
2022-03-01 12:32:58 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,6 +1,16 @@
 #print([obj1],[obj2]...[,sep=""][,"endl=""][,file=sys.stdout])
+
+print("a","b","c")
 print("a", "b", "c", sep="")
+print("a", "b", "c", sep="#")
+#比较上面三个打印的区别
+
+print("abc")
 print("def", end="")
+print("g")
+#观察有end与没有的区别
+
 wf = open("t.txt", "w")
 print("123", file=wf)#t.txt写入“123”
 wf.close()
+#观察项目文件夹下是否生成t.txt,内容是否为"123"
